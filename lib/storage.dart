fa93df00e134b9aa9de228d7cc98c2fcf5a41558
Luca De Lauw <lucadelauw@gmail.com>
2022-05-02 20:18:37 +0200

    rework localstorage to firestore

--- lib/storage.dart
+++ lib/storage.dart
@@ -1,45 +1,41 @@
 import 'dart:developer';
+import 'package:mobileintro/firebase_options.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Storage {
+
+  static bool inited = false;
+
   static Future<void> setStudents(Map<int, String> students) async {
-    final prefs = await SharedPreferences.getInstance();
 
-    var studentNames = <String>[];
-    var studentNumbers = <String>[];
-
-    for (int key in students.keys) {
-      studentNames.add(students[key]!);
-      studentNumbers.add(key.toString());
+    if (!inited) {
+      await init();
     }
 
-    await prefs.setStringList("StudentNumbers", studentNumbers);
-    await prefs.setStringList("StudentNames", studentNames);
-
-    return;
+    await FirebaseFirestore.instance.collection('E-xam').doc('students').set(students.map((key, value) => MapEntry(key.toString(), value)));
   }
 
   static Future<Map<int, String>> getStudents() async {
-    final prefs = await SharedPreferences.getInstance();
 
-    final List<String> numbers = prefs.getStringList("StudentNumbers")!.toList();
-    final List<String> names = prefs.getStringList("StudentNames")!.toList();
+    if (!inited) {
+      await init();
+    }
 
     var students = <int, String>{};
 
-    log(numbers.length.toString());
-    log(names.length.toString());
-
-    if (numbers.length != names.length) {
-      throw Exception("StudentName length not equal to StudentNumber length");
-    }
-
-    for (var i = 0; i < numbers.length; i++) {
-      students[int.parse(numbers[i])] = names[i];
-    }
+    await FirebaseFirestore.instance.collection('E-xam').doc('students').get().then((value) => {
+      students = value.data()!.map((key, value) => MapEntry(int.parse(key), value.toString()))
+    });
 
     return students;
+  }
+
+  static Future<void> init() async {
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    return;
   }
 }
 
