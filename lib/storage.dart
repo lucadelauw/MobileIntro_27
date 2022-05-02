261ec9bf014a62e8541c47f11c16fafd70c387c8
Luca De Lauw <lucadelauw@gmail.com>
2022-05-02 22:47:05 +0200

    Storage add questions + offline data

--- lib/storage.dart
+++ lib/storage.dart
@@ -9,29 +9,24 @@ class Storage {
 
   static bool inited = false;
 
-  static Future<void> setStudents(Map<int, String> students) async {
-
-    if (!inited) {
-      await init();
-    }
-
-    await FirebaseFirestore.instance.collection('E-xam').doc('students').set(students.map((key, value) => MapEntry(key.toString(), value)));
-  }
-
   static Future<void> addStudent(int number, String name) async {
-    await FirebaseFirestore.instance.collection('students').where('number', isEqualTo: number).get().then((value) async => {
+    await FirebaseFirestore.instance.collection('students').where(
+        'number', isEqualTo: number).get().then((value) async =>
+    {
       if (value.size == 0) {
-        await FirebaseFirestore.instance.collection('students').add({'name': name, 'number': number})
+        await FirebaseFirestore.instance.collection('students').add(
+            {'name': name, 'number': number})
       }
     });
   }
 
   static Future<void> removeStudent(int number) async {
-    await FirebaseFirestore.instance.collection('students').where('number', isEqualTo: number).get().then((value) => value.docs.first.reference.delete());
+    await FirebaseFirestore.instance.collection('students').where(
+        'number', isEqualTo: number).get().then((value) =>
+        value.docs.first.reference.delete());
   }
 
   static Future<Map<int, String>> getStudents() async {
-
     if (!inited) {
       await init();
     }
@@ -42,7 +37,9 @@ class Storage {
       students = value.data()!.map((key, value) => MapEntry(int.parse(key), value.toString()))
     });*/
 
-    await FirebaseFirestore.instance.collection('students').get().then((value) => {
+    await FirebaseFirestore.instance.collection('students').get().then((
+        value) =>
+    {
       value.docs.forEach((element) {
         students[element.get('number')] = element.get('name');
       })
@@ -53,13 +50,48 @@ class Storage {
 
   static Future<void> init() async {
     await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    await FirebaseFirestore.instance.enablePersistence();
+    inited = true;
     return;
+  }
+
+  static Future<void> setMultipleChoiceQuestion(int questionNumber, MultipleChoiceQuestion questionData) async {
+    await FirebaseFirestore.instance.collection('questions').doc(
+        questionNumber.toString()).set({'type': 'MultipleChoice', 'input': questionData.input, 'answer': questionData.answer});
+  }
+
+  static Future<void> setOpenQuestion(int questionNumber, OpenQuestion questionData) async {
+    await FirebaseFirestore.instance.collection('questions').doc(
+        questionNumber.toString()).set({'type': 'Open', 'input': questionData.input, 'answer': questionData.answer});
+  }
+
+  static Future<void> setCodeCorrectionQuestion(int questionNumber, CodeCorrectionQuestion questionData) async {
+    await FirebaseFirestore.instance.collection('questions').doc(
+        questionNumber.toString()).set({'type': 'CodeCorrection', 'input': questionData.input, 'answer': questionData.answer});
   }
 }
 
-class StudentObject{
-  int studentNumber;
-  String name;
+class MultipleChoiceQuestion {
+  List<String> input = [];
+  String answer = "";
 
-  StudentObject(this.studentNumber, this.name);
+  MultipleChoiceQuestion(this.input, this.answer) {
+    if (!input.contains(answer)) {
+      throw("MultipleChoiceQuestion has no correct answer");
+    }
+  }
+}
+
+class OpenQuestion {
+  String input = "";
+  String? answer = "";
+
+  OpenQuestion(this.input, this.answer);
+}
+
+class CodeCorrectionQuestion {
+  String input = "";
+  String answer = "";
+
+  CodeCorrectionQuestion(this.input, this.answer);
 }
